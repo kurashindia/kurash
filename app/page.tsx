e46dcade38735497ce15cea296ca4903b1b6c65e
kurashindia <tikakurash@gmail.com>
2025-07-04 11:46:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-// app/(dashboard)/events/[id]/sub-events/[subEventId]/page.tsx
 import { redirect } from 'next/navigation';
 
-interface PageProps {
-  params: {
-    id: string;
-    subEventId: string;
-  };
-}
-
-export default function SubEventPage({ params }: PageProps) {
+export default function Home() {
   redirect('/dashboard');
 }
